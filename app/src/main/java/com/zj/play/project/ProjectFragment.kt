9abd4c49dd56a4956230e56602edacce18667068
prisar <pritimaysarkar2010@gmail.com>
2022-06-24 22:04:35 +0530

    play

---
+++ app/src/main/java/com/zj/play/project/ProjectFragment.kt
@@ -0,0 +1,76 @@
+package com.zj.play.project
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.android.material.tabs.TabLayoutMediator
+import com.zj.core.view.custom.FragmentAdapter
+import com.zj.play.databinding.FragmentProjectBinding
+import com.zj.play.project.list.ProjectListFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProjectFragment : BaseTabFragment() {
+
+    private val viewModel by viewModels<ProjectViewModel>()
+
+    private lateinit var adapter: FragmentAdapter
+    private var binding: FragmentProjectBinding? = null
+
+    override fun getLayoutView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        attachToRoot: Boolean
+    ): View {
+        binding = FragmentProjectBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun initView() {
+        adapter = FragmentAdapter(requireActivity().supportFragmentManager, lifecycle)
+        binding?.apply {
+            projectViewPager2?.adapter = adapter
+            projectTabLayout.addOnTabSelectedListener(this@ProjectFragment)
+            TabLayoutMediator(projectTabLayout, projectViewPager2!!) { tab, position ->
+                tab.text = adapter.title(position)
+            }.attach()
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun initData() {
+        startLoading()
+        setDataStatus(viewModel.dataLiveData) {
+            val nameList = mutableListOf<String>()
+            val viewList = mutableListOf<Fragment>()
+            it.forEach { project ->
+                nameList.add(project.name)
+                viewList.add(ProjectListFragment.newInstance(project.id))
+            }
+            adapter.apply {
+                reset(nameList.toTypedArray())
+                reset(viewList)
+                notifyDataSetChanged()
+            }
+            binding?.projectViewPager2?.currentItem = viewModel.position
+        }
+        getProjectTree()
+    }
+
+    private fun getProjectTree() {
+        viewModel.getDataList(false)
+    }
+
+    override fun onTabPageSelected(position: Int) {
+        viewModel.position = position
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ProjectFragment()
+    }
+
+}
